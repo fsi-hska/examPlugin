350a737b63963aaa4a9b455431da15220050b5c8
Florian Rinke <florian.rinke@fs-eit.de>
2012-03-10 12:26:14 +0100

    fehlende Änderungen im Syntax-Plugin ergänzt

--- syntax/main.php
+++ syntax/main.php
@@ -30,7 +30,7 @@ class syntax_plugin_klausuren_main extends DokuWiki_Syntax_Plugin {
 	}
 
 	function connectTo($mode) {
-		$this->Lexer->addSpecialPattern('\{\{klausuren>[A-Za-z0-9]+?\}\}',$mode,'plugin_klausuren_main');
+		$this->Lexer->addSpecialPattern('\{\{klausuren>(?:\w{1,3}\/)?\w+?(?:>\w+?)?\}\}',$mode,'plugin_klausuren_main');
 	}
 
 	/**
@@ -40,8 +40,11 @@ class syntax_plugin_klausuren_main extends DokuWiki_Syntax_Plugin {
 	function handle($match, $state, $pos, &$handler){
 
 		// Grep for lesson
-		$lesson = substr($match, 12, -2);
-		return array('lesson' => $lesson);
+		preg_match('/\{\{klausuren>(?:(\w{1,3})\/)?(\w+?)(?:>(\w+?))?\}\}/',$match,$data);
+		$lesson = $detail[1];
+		$course = $detail[2];
+		$doctype= isset($detail[3])?isset($detail[3]):'klausur';
+		return array('course' => $data[1], 'lesson' => $data[2], 'doctype' => $data[3]);
 
 	}
 
@@ -52,12 +55,12 @@ class syntax_plugin_klausuren_main extends DokuWiki_Syntax_Plugin {
 		if($mode != 'xhtml')
 			return false;
 
-		for($i=1;$i < $renderer->lastlevel;$i++) {
+		/*for($i=1;$i < $renderer->lastlevel;$i++) {
 			//$renderer->doc .= '</div>';
 			$renderer->section_close();
-		}
+		}*/
 
-		$renderer->doc .= '<h2>Klausuren</h2>';
+		//$renderer->doc .= '<h2>Klausuren</h2>';
 		
 		// Show upload form if user is allowed to upload here
 		if(auth_quickaclcheck($this->getConf('unterlagenNS').'/'.$data['lesson'].':*') >= AUTH_UPLOAD) {
@@ -69,7 +72,7 @@ class syntax_plugin_klausuren_main extends DokuWiki_Syntax_Plugin {
 		
 		// Shows the list of examns
 		$downhelper =& plugin_load('helper','klausuren_download');
-		$renderer->section_open(2);
+		//$renderer->section_open(2);
 		$downhelper->output(&$renderer, $data);
 
 		return true;
