440e9a482258779ed0c3b3b6cf1a7a34d04e4711
memo@mittelmeer <memo_23@gmx.de>
2016-01-21 18:01:36 +0100

    Temporary quick fix for Issue #14 - increased amount of semesters shown

--- helper/upload.php
+++ helper/upload.php
@@ -34,8 +34,8 @@ class helper_plugin_klausuren_upload extends Dokuwiki_Plugin {
 		$form->addElement(form_makeFileField('upload', "", 'upload_file', '',
 		   	array('accept' => 'application/pdf')));
 		$form->addElement('<br>');
-		$form->addElement(form_makeListboxField('semester', $helper->getLastSemesters(20), '', ''));
-		$form->addElement(form_makeListboxField('type', 
+		$form->addElement(form_makeListboxField('semester', $helper->getLastSemesters(30), '', ''));
+		$form->addElement(form_makeListboxField('type',
 				array('klausur' => 'Klausur', 'loesung' => 'Lösung', 'klausur_loesung' => 'Klausur + Lösung'),
 			'', ''));
 		$form->addElement(form_makeButton('submit', '', 'Hochladen'));
